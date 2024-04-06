574294e085156726fb35239e86e9b1760f39b1c7
= <=>
2024-04-07 07:56:33 +0800

    full popmodal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import "./App.css";
 import NiiModal from "./NiiModal";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [openmodal, setOpenModal] = useState("hide");
 
+  const notify = () => toast.success('Bet submitted successfully', {
+    style: {
+      border: '1px solid #ffc61c',
+      padding: '16px',
+      color: '#713200',
+    },
+    iconTheme: {
+      primary: '#ffd445',
+      secondary: '#FFFAEE',
+    },
+  });
+
   const handleClick = (e) => {
     setOpenModal((holder) => {
-      return holder == "show" ? "hide" : "show";
+      return holder === "show" ? "hide" : "show";
     });
   };
 
   const closeModal = () => {
-    setOpenModal("hide");
+    setOpenModal((holder) => {
+      return holder === "show" ? "hide" : "show";
+    });
   };
 
   return (
     <>
+      <div>
+        <Toaster position="bottom-center" />
+      </div>
       <button onClick={handleClick}>Show Nii-Modal</button>
-      {openmodal === 'show' ? <NiiModal closemodal={closeModal} modalstatus={openmodal} /> : null}
+      {openmodal === "show" ? (
+        <NiiModal closemodal={closeModal} modalstatus={openmodal} toast={notify} />
+      ) : (
+        <div>Custom React-Nii POPModal V1.0</div>
+      )}
       {/* {<NiiModal closemodal={closeModal} modalstatus={openmodal}/>} */}
     </>
   );
--- src/NiiModal.js
+++ src/NiiModal.js
@@ -1,13 +1,22 @@
-import Bounce from 'bounce.js';
+import Bounce from "bounce.js";
 import headerlogo from "./flat.png";
+import React, { useState, useEffect } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
+import { FaDice } from "react-icons/fa6";
 import { IoCloseCircleSharp } from "react-icons/io5";
-import React, { useEffect } from "react";
+
+const cssprops = {
+  // add more properties if you wish kwame
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 const NiiModal = (props) => {
-
+  let [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (props.modalstatus == 'show') {
+    if (props.modalstatus === "show") {
       const bounce = new Bounce();
       bounce.scale({
         from: { x: 0.5, y: 0.5 },
@@ -17,48 +26,60 @@ const NiiModal = (props) => {
     }
   }, []);
 
-  console.log(props)
-
   const closeModal = () => {
     props.closemodal();
+    setLoading(false);
+  };
+
+  const confirmBtnClicked = () => {
+    setLoading(true);
+    // Simulate an asynchronous action (e.g., API call) here
+    setTimeout(() => {
+      setLoading(false);
+      props.toast(); // Trigger toast after action completion
+      props.closemodal(); // Close modal after action completion
+    }, 3000);
   };
 
   return (
     <>
       <div className="overlay" onClick={closeModal}></div>
       <div className="cover">
-      <div className="modal">
-        <div className="header">
-          <img src={headerlogo} className="headerlogo" alt="logo" />
-        </div>
-        <div className="icondiv">
-          <span className="title">Confirm</span>
-          <IoCloseCircleSharp className="icon" size={25} />
-        </div>
-        <div className="content">
-          <div className="container">
-            <div className="betdetails">
-              <span>Bet Type: </span>
-              <span>Single</span>
-            </div>
-            <div className="betdetails">
-              <span>Booking Code: </span>
-              <span>B3CRTMT </span>
-            </div>
-            <div className="table">
-              <span>Game selections goes here</span>
+        <div className="modal">
+          <div className="header">
+            <img src={headerlogo} className="headerlogo" alt="logo" />
+          </div>
+          <div className="icondiv">
+            <span className="title">Confirm</span>
+            <IoCloseCircleSharp className="icon" onClick={closeModal} size={25} />
+          </div>
+          <div className="content">
+            <div className="container">
+              <div className="betdetails">
+                <span>Bet Type: </span>
+                <span>Single</span>
+              </div>
+              <div className="betdetails">
+                <span>Booking Code: </span>
+                <span>B3CRTMT </span>
+              </div>
+              <div className="table" >
+                <FaDice size={70} color="#ddd"/>
+              </div>
             </div>
           </div>
+          <div className="footer">
+            <button className="cancelbtn actionbtn" onClick={closeModal}>
+              Cancel
+            </button>
+            <button
+              className="confirmbtn actionbtn"
+              onClick={confirmBtnClicked}
+            >
+              {loading === true ? <BeatLoader color="#ffc71f" /> : "Confirm"}
+            </button>
+          </div>
         </div>
-        <div className="footer">
-          <button className="cancelbtn actionbtn" onClick={closeModal}>
-            Cancel
-          </button>
-          <button className="confirmbtn actionbtn" onClick={closeModal}>
-            Confirm
-          </button>
-        </div>
-      </div>
       </div>
     </>
   );
